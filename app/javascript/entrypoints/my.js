bee7de41554fa8639de088ee285ab4fbcf07cb8c
fernando-vunge <developervunge@gmail.com>
2024-11-06 14:06:53 +0000

    the first commit

---
+++ app/javascript/entrypoints/my.js
@@ -0,0 +1,7 @@
+// Seleciona o botão pelo ID
+const button = document.getElementById('o');
+
+// Adiciona um evento de clique ao botão
+button.addEventListener('click', function() {
+  console.log('hi')
+});
